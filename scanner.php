e61f47811097d62d3d2f7662d176bb547fd2b70c
juniar <muhammadjuniar77@gmail.com>
2023-11-07 15:45:55 +0700

    change: margin top scanner

--- scanner.php
+++ scanner.php
@@ -1,6 +1,6 @@
 <?php include "./components/header.php"; ?>
 <body style="background: #fff;">
-    <div class="container text-center" style="margin-top: 40%">
+    <div class="container text-center" style="margin-top: 20px">
         <div class="align-self-center">
             <div id="qr-reader"></div>
         </div>
