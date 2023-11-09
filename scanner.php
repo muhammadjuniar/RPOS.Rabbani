a5d0446ec7811920ed8c438bdb7d4eb772c7941c
juniar <muhammadjuniar77@gmail.com>
2023-11-09 15:50:55 +0700

    update: scanner.php

--- scanner.php
+++ scanner.php
@@ -9,4 +9,14 @@
         </div>
     </div>
 
+    <script>
+        function onScanSuccess(decodedText, decodedResult) {
+            // console.log(`Code scanned = ${decodedText}`, decodedResult);
+            // document.getElementById('result').innerHTML = decodedText;
+            window.location.replace(`stockopname.php?keyword=${decodedText}`);
+        }
+        var html5QrcodeScanner = new Html5QrcodeScanner(
+            "qr-reader", { fps: 10, qrbox: 250 });
+        html5QrcodeScanner.render(onScanSuccess);
+    </script>
 <?php include "./components/footer.php"; ?>
