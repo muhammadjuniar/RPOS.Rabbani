4bfcec6a4bd6a2a8a7016139e8c4aedd15bcec35
juniar <muhammadjuniar77@gmail.com>
2023-11-13 11:02:12 +0700

    update: nav icon halaman laporan

--- report.php
+++ report.php
@@ -7,45 +7,47 @@
         <div class="container">
 
             <!-- Tabs -->
-            <ul class="nav nav-justified nav-pills mb-3" id="pills-tab" role="tablist">
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link active d-flex align-items-center" id="pills-pembelian-tab" data-bs-toggle="pill" data-bs-target="#pills-pembelian" type="button" role="tab" aria-controls="pills-pembelian" aria-selected="true">
-                        <img class="inactive-img" src="assets/images/menu_lap_beli.svg" alt="">
-                        <img class="active-img" src="assets/images/menu_lap_beli_active.svg" alt=""><span>Pembelian</span>
-                    </button>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link d-flex align-items-center" id="pills-penjualan-tab" data-bs-toggle="pill" data-bs-target="#pills-penjualan" type="button" role="tab" aria-controls="pills-penjualan" aria-selected="false">
-                        <img class="inactive-img" src="assets/images/menu_lap_jual.svg" alt="">
-                        <img class="active-img" src="assets/images/menu_lap_jual_active.svg" alt=""><span>Penjualan</span>
-                    </button>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link d-flex align-items-center" id="pills-retur-pembelian-tab" data-bs-toggle="pill" data-bs-target="#pills-retur-pembelian" type="button" role="tab" aria-controls="pills-retur-pembelian" aria-selected="false">
-                        <img class="inactive-img" src="assets/images/menu_lap_retur_beli.svg" alt="">
-                        <img class="active-img" src="assets/images/menu_lap_retur_beli_active.svg" alt=""><span>Retur Pembelian</span>
-                    </button>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link d-flex align-items-center" id="pills-retur-penjualan-tab" data-bs-toggle="pill" data-bs-target="#pills-retur-penjualan" type="button" role="tab" aria-controls="pills-retur-penjualan" aria-selected="false">
-                        <img class="inactive-img" src="assets/images/menu_lap_retur_jual.svg" alt="">
-                        <img class="active-img" src="assets/images/menu_lap_retur_jual_active.svg" alt=""><span>Retur Penjualan</span>
-                    </button>
-                </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link d-flex align-items-center" id="pills-stock-opname-tab" data-bs-toggle="pill" data-bs-target="#pills-stock-opname" type="button" role="tab" aria-controls="pills-stock-opname" aria-selected="false">
-                        <img class="inactive-img" src="assets/images/menu_so.svg" alt="">
-                        <img class="active-img" src="assets/images/menu_so_active.svg" alt=""><span>Stock Opname</span>
-                    </button>
-                </li>
-            </ul>
+            <div class="container fixed-top">
+                <ul class="nav nav-justified nav-pills mb-3" id="pills-tab" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link active d-flex align-items-center" id="pills-pembelian-tab" data-bs-toggle="pill" data-bs-target="#pills-pembelian" type="button" role="tab" aria-controls="pills-pembelian" aria-selected="true">
+                            <img class="inactive-img" src="assets/images/menu_lap_beli.svg" alt="">
+                            <img class="active-img" src="assets/images/menu_lap_beli_active.svg" alt=""><span>Pembelian</span>
+                        </button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link d-flex align-items-center" id="pills-penjualan-tab" data-bs-toggle="pill" data-bs-target="#pills-penjualan" type="button" role="tab" aria-controls="pills-penjualan" aria-selected="false">
+                            <img class="inactive-img" src="assets/images/menu_lap_jual.svg" alt="">
+                            <img class="active-img" src="assets/images/menu_lap_jual_active.svg" alt=""><span>Penjualan</span>
+                        </button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link d-flex align-items-center" id="pills-retur-pembelian-tab" data-bs-toggle="pill" data-bs-target="#pills-retur-pembelian" type="button" role="tab" aria-controls="pills-retur-pembelian" aria-selected="false">
+                            <img class="inactive-img" src="assets/images/menu_lap_retur_beli.svg" alt="">
+                            <img class="active-img" src="assets/images/menu_lap_retur_beli_active.svg" alt=""><span>Retur Pembelian</span>
+                        </button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link d-flex align-items-center" id="pills-retur-penjualan-tab" data-bs-toggle="pill" data-bs-target="#pills-retur-penjualan" type="button" role="tab" aria-controls="pills-retur-penjualan" aria-selected="false">
+                            <img class="inactive-img" src="assets/images/menu_lap_retur_jual.svg" alt="">
+                            <img class="active-img" src="assets/images/menu_lap_retur_jual_active.svg" alt=""><span>Retur Penjualan</span>
+                        </button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link d-flex align-items-center" id="pills-stock-opname-tab" data-bs-toggle="pill" data-bs-target="#pills-stock-opname" type="button" role="tab" aria-controls="pills-stock-opname" aria-selected="false">
+                            <img class="inactive-img" src="assets/images/menu_so.svg" alt="">
+                            <img class="active-img" src="assets/images/menu_so_active.svg" alt=""><span>Stock Opname</span>
+                        </button>
+                    </li>
+                </ul>
+            </div>
             <!-- Tabs -->
 
             <div class="tab-content" id="pills-tabContent">
                 
                 <!-- pembelian-tab -->
                 <div class="tab-pane fade show active" id="pills-pembelian" role="tabpanel" aria-labelledby="pills-pembelian-tab">
-                    <div class="row info-tab">
+                    <div class="row info-tab text-start">
                         <div class="col-12 separator">&nbsp;</div>
                         <div class="col-6">
                             <input class="form-control custom-date" value="<?php echo date("Y-m-d") ?>" type="date">
