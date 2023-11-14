76c0b22542a389159d9b80a4f32efdab2afd5f38
juniar <muhammadjuniar77@gmail.com>
2023-11-14 13:32:22 +0700

    update: navbar dan iconnya

--- components/navbar.php
+++ components/navbar.php
@@ -19,32 +19,14 @@
                 <ul class="navbar-nav nav-justified w-100">
                 <li class="nav-item">
                     <a href="index.php" class="nav-link text-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                            <g clip-path="url(#clip0_2303_330)">
-                                <path d="M10 20V14H14V20H19V12H22L12 3L2 12H5V20H10Z" fill="#FF99CC"/>
-                            </g>
-                            <defs>
-                                <clipPath id="clip0_2303_330">
-                                    <rect width="24" height="24" fill="white"/>
-                                </clipPath>
-                            </defs>
-                        </svg>
+                        <img src="assets/images/nav_home.svg" alt="">
                         <span class="small d-block pt-1">Home</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="#" class="nav-link text-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                            <g clip-path="url(#clip0_2303_326)">
-                                <path d="M19 3H4.99C3.88 3 3.01 3.89 3.01 5L3 19C3 20.1 3.88 21 4.99 21H19C20.1 21 21 20.1 21 19V5C21 3.89 20.1 3 19 3ZM19 15H15C15 16.66 13.65 18 12 18C10.35 18 9 16.66 9 15H4.99V5H19V15Z" fill="#FF99CC"/>
-                            </g>
-                            <defs>
-                                <clipPath id="clip0_2303_326">
-                                    <rect width="24" height="24" fill="white"/>
-                                </clipPath>
-                            </defs>
-                        </svg>
-                        <span class="small d-block pt-1">Inbox</span>
+                        <img src="assets/images/nav_shopping_cart.svg" alt="">
+                        <span class="small d-block pt-1">Order</span>
                     </a>
                 </li>
                 <li class="nav-item">
@@ -52,24 +34,13 @@
                 </li>
                 <li class="nav-item">
                     <a href="#" class="nav-link text-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="18" viewBox="0 0 24 18" fill="none">
-                            <path d="M22.5 0.75H8.25C8.0505 0.75 7.86 0.8295 7.719 0.969L6.75 1.9395L5.781 0.969C5.64 0.8295 5.4495 0.75 5.25 0.75H1.5C0.6735 0.75 0 1.4235 0 2.25V15.75C0 16.578 0.6735 17.25 1.5 17.25H5.25C5.4495 17.25 5.64 17.1705 5.781 17.031L6.75 16.0605L7.719 17.0295C7.86 17.1705 8.0505 17.25 8.25 17.25H22.5C23.328 17.25 24 16.578 24 15.75V2.25C24 1.4235 23.328 0.75 22.5 0.75ZM7.5 14.25H6V12.75H7.5V14.25ZM7.5 11.25H6V9.75H7.5V11.25ZM7.5 8.25H6V6.75H7.5V8.25ZM7.5 5.25H6V3.75H7.5V5.25ZM12.75 3.75C13.9905 3.75 15 4.7595 15 6C15 7.2405 13.9905 8.25 12.75 8.25C11.5095 8.25 10.5 7.2405 10.5 6C10.5 4.7595 11.5095 3.75 12.75 3.75ZM11.25 14.25C11.0805 14.25 10.911 14.193 10.77 14.076C10.452 13.8105 10.4085 13.338 10.674 13.02L18.174 4.02C18.441 3.7005 18.9135 3.6615 19.23 3.924C19.548 4.1895 19.59 4.662 19.3245 4.9815L11.8245 13.9815C11.6775 14.157 11.4645 14.25 11.25 14.25ZM17.25 14.25C16.0095 14.25 15 13.2405 15 12C15 10.7595 16.0095 9.75 17.25 9.75C18.4905 9.75 19.5 10.7595 19.5 12C19.5 13.2405 18.4905 14.25 17.25 14.25Z" fill="#FF99CC"/>
-                        </svg>
+                        <img src="assets/images/nav_promo.svg" alt="">
                         <span class="small d-block pt-1">Promo</span>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="#" class="nav-link text-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="24" viewBox="0 0 24 24" fill="none">
-                            <g clip-path="url(#clip0_2303_311)">
-                                <path d="M12 11.7544C14.21 11.7544 16 10.0011 16 7.83628C16 5.67152 14.21 3.91815 12 3.91815C9.79 3.91815 8 5.67152 8 7.83628C8 10.0011 9.79 11.7544 12 11.7544ZM12 13.7135C9.33 13.7135 4 15.0261 4 17.6316V19.5907H20V17.6316C20 15.0261 14.67 13.7135 12 13.7135Z" fill="#FF99CC"/>
-                            </g>
-                            <defs>
-                                <clipPath id="clip0_2303_311">
-                                    <rect width="24" height="23.5088" fill="white"/>
-                                </clipPath>
-                            </defs>
-                        </svg>
+                        <img src="assets/images/nav_user.svg" alt="">
                         <span class="small d-block pt-1">Akun</span>
                     </a>
                 </li>
